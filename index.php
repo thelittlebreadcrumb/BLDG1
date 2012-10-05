cdd8ec095323889c08c39818178eb2a7e85f9d5e
Bhavya Daiya <bhavya@bhavzproject.com>
2012-10-05 22:35:16 +1300

    Initial Release

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'init.php';
 include './template/header.php';
 
 if (logged_in()) {
-	echo 'Welcome, you can now start <a href="create_albums.php">create albums</a> and <a href="upload_image.php">upload images</a>';
+	echo 'Welcome, you can now start <a href="">create albums</a> and <a href="">upload images</a>';
 } else {
 echo '<img src="images/landing.png" alt="Register a free account today" />';
 }
